f518e1fe283f975991b440a8c76931cea13a7fdc
ninuxGithub <1732768613@qq.com>
2017-07-26 10:46:26 +0800

    add mockmvc test

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.bean.Order;
@@ -39,6 +40,13 @@ public class OrderController {
 	
 	private String contextPath;
 
+	@ResponseBody
+	@RequestMapping("/orderList")
+	public List<Order> orderList() {
+		List<Order> orderList = orderRepository.findAll();
+		return orderList;
+	}
+	
 	@RequestMapping("/orders")
 	public String loadOrder(Model model , HttpServletRequest request) {
 		List<Order> orderList = orderRepository.findAll();
--- src/test/java/com/example/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/example/demo/DemoProjectApplicationTests.java
@@ -1,16 +1,51 @@
 package com.example.demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class DemoProjectApplicationTests {
+	private MockMvc mvc;
+
+	MockHttpSession session;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Before
+	public void setupMockMvc() {
+		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+		session = new MockHttpSession();
+	}
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		RequestBuilder request = get("/orderList");
+		mvc.perform(request).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andDo(MockMvcResultHandlers.print())
+				.andReturn();
+		mvc.perform(get("/orders/")).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+		log.info("info....");
+		// mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
 	}
 
 }
